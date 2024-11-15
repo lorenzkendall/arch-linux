0936aa32e724ec70e16673f61a90f2bee9c8e1b1
megap1nt <150217651+megap1nt@users.noreply.github.com>
2024-11-15 07:22:15 -0600

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,4 +1,20 @@
-# 
+# Bluetooth setup
+sudo pacman -S bluez bluez-utils
+bluetoothctl start bluetooth.service
+bluetoothctl enable bluetooth.service
+
+# Pair bluetooth via terminal 
+bluetoothctl
+power on 
+default-agent
+scan on 
+# Find your device and copy MAC address
+trust <mac-address> # Replace <mac-address> with your device's MAC address
+pair <mac-address> # Replace <mac-address> with your device's MAC address
+connect <mac-address> # Replace <mac-address> with your device's MAC address
+scan off
+sudo vim /etc/bluetooth/main.conf 
+# Change "AutoEnable=false" to "true" 
 
 # Install Reflector to get the fastest mirrors for pacman 
 sudo pacman -S reflector 
